1c4af9698ccbb173b7531507a8ad657ca3823e8a
vandana7024 <vandanadewangan1999@gmail.com>
2022-04-18 15:39:51 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        doctor: "url('/Pages/doctor.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
